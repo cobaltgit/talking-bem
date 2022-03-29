8ccbed3b6174251cd780ee392b592a2c15a3eee6
cobaltgit <criterion@chitco.co.uk>
2022-03-29 19:41:44 +0100

    Restructure tasks

--- cogs/background.py
+++ cogs/background.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands, tasks
+from itertools import cycle
 
 
 class BackgroundTasks(commands.Cog):
@@ -7,17 +8,22 @@ class BackgroundTasks(commands.Cog):
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        self.presences = cycle([
+            f"Taking calls in {len(self.bot.guilds)} guilds", 
+            "Vote me on top.gg! https://top.gg/bot/955078353700937778", 
+            "Source code: https://github.com/cobaltgit/talking-ben"
+        ])
+        self.tasks = [self.update_presence]
 
-    @commands.Cog.listener()
-    async def on_ready(self) -> None:
-        self.update_presence.start()
+    async def cog_load(self) -> None:
+        for t in self.tasks:
+            t.start()
 
-    @tasks.loop(seconds=10)
+    @tasks.loop(seconds=45)
     async def update_presence(self) -> None:
         await self.bot.change_presence(
-            activity=discord.Activity(name=f"over {len(self.bot.guilds)} guilds", type=discord.ActivityType.watching)
+            activity=discord.Activity(name=next(self.presences), type=discord.ActivityType.watching)
         )
-
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(BackgroundTasks(bot))
