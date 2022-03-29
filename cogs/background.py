1a7022f1df033bc2629b8ae8b51d0f6fa93ef8e2
cobaltgit <criterion@chitco.co.uk>
2022-03-29 20:07:15 +0100

    Reset bc I suck + ratio + cope

--- cogs/background.py
+++ cogs/background.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands, tasks
-from itertools import cycle
 
 
 class BackgroundTasks(commands.Cog):
@@ -8,22 +7,17 @@ class BackgroundTasks(commands.Cog):
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self.presences = cycle([
-            f"Taking calls in {len(bot.guilds)} guilds", 
-            "Vote me on top.gg! https://top.gg/bot/955078353700937778", 
-            "Source code: https://github.com/cobaltgit/talking-ben"
-        ])
         self.tasks = [self.update_presence]
 
     async def cog_load(self) -> None:
         for t in self.tasks:
             t.start()
 
-    @tasks.loop(seconds=30)
+    @tasks.loop(seconds=10)
     async def update_presence(self) -> None:
         await self.bot.wait_until_ready()
         await self.bot.change_presence(
-            activity=discord.Activity(name=next(self.presences), type=discord.ActivityType.watching)
+            activity=discord.Activity(name=f"over {len(self.bot.guilds)} guilds", type=discord.ActivityType.watching)
         )
 
 async def setup(bot: commands.Bot) -> None:
