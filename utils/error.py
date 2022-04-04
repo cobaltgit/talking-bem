d55ccbde80a1d5e7861b3dcc008cd672cf72321c
cobaltgit <criterion@chitco.co.uk>
2022-04-04 20:07:20 +0100

    Reformat

--- utils/error.py
+++ utils/error.py
@@ -1,21 +1,20 @@
 from typing import Union
-from discord import app_commands
-from discord import Interaction
+
+from discord import Interaction, app_commands
 from discord.app_commands import AppCommandError, Command, ContextMenu
 
-async def on_app_command_error(
-    interaction: Interaction,
-    command: Union[Command, ContextMenu],
-    error: AppCommandError
-):
+
+async def on_app_command_error(interaction: Interaction, command: Union[Command, ContextMenu], error: AppCommandError):
     error = getattr(error, "original", error)
-    
+
     match error.__class__:
         case app_commands.CommandNotFound:
             return
         case app_commands.MissingPermissions:
-            msg = "__This command requires the following permissions:__\n**{}**".format('\n'.join(i.replace('_', ' ').replace('guild', 'server').title() for i in error.missing_permissions))
+            msg = "__This command requires the following permissions:__\n**{}**".format(
+                "\n".join(i.replace("_", " ").replace("guild", "server").title() for i in error.missing_permissions)
+            )
         case app_commands.CheckFailure:
             msg = str(error)
-            
+
     return await interaction.response.send_message(msg, ephemeral=True)
