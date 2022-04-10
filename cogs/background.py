098d593d2e8977e62c9f3d0b73d27d034e587143
cobaltgit <criterion@chitco.co.uk>
2022-04-10 21:13:05 +0100

    Cycle through statuses

--- cogs/background.py
+++ cogs/background.py
@@ -1,3 +1,4 @@
+from itertools import cycle
 import discord
 from discord.ext import commands, tasks
 
@@ -8,16 +9,26 @@ class BackgroundTasks(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
         self.tasks = [self.update_presence]
+        self._presences = [
+            "over {guild_count} servers",
+            "support server - https://discord.gg/cq4VawvptQ",
+            "top.gg - https://top.gg/bot/955078353700937778",
+            "source code - https://github.com/cobaltgit/talking-ben"
+        ]
+        self.presences = cycle(self._presences)
 
     async def cog_load(self) -> None:
         for t in self.tasks:
             t.start()
 
-    @tasks.loop(seconds=10)
+    @tasks.loop(seconds=30)
     async def update_presence(self) -> None:
         await self.bot.wait_until_ready()
+        FORMATS = {
+            "guild_count": len(self.bot.guilds),
+        }
         await self.bot.change_presence(
-            activity=discord.Activity(name=f"over {len(self.bot.guilds)} guilds", type=discord.ActivityType.watching)
+            activity=discord.Activity(name=next(self.presences).format(**FORMATS), type=discord.ActivityType.watching)
         )
 
 
